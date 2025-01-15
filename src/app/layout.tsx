ca70d8b29557164dab8993eeeb1f3cd4ea79ff2f
sum-in-e <suminkim.me@gmail.com>
2025-01-15 19:01:25 +0900

    feat: 구글 태그매니저 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Providers from '@/components/providers'
 import { pretendardFont } from '@/lib/fonts'
 import { Metadata } from 'next'
 import { Toaster } from 'sonner'
+import { GoogleTagManager } from '@next/third-parties/google'
 
 // TODO: 메타데이터에 필요한 데이터 요청
 export const metadata: Metadata = {
@@ -22,8 +23,10 @@ export default function RootLayout({
     <html lang="ko" className={`${pretendardFont.variable} font-sans`} suppressHydrationWarning>
       <body suppressHydrationWarning>
         {/* TODO: 393px screen 스타일 적용해서 싹 바꾸기 */}
+        {process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_GA_ID && (
+          <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID} />
+        )}
         <Providers>{children}</Providers>
-
         <Toaster toastOptions={{ unstyled: true }} duration={2000} />
       </body>
     </html>
