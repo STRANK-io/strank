e20212e6471e2da559929fe6b32c5631965e9965
sum-in-e <suminkim.me@gmail.com>
2025-01-24 21:58:05 +0900

    fix: 롤백

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -3,16 +3,14 @@ import PrivatePageNav from '@/components/common/PrivatePageNav'
 import { UserProvider } from '@/contexts/UserContext'
 import { ERROR_CODES } from '@/lib/constants/error'
 import { ROUTES } from '@/lib/constants/routes'
-import { createClient } from '@/lib/supabase/server'
 import { redirectWithError } from '@/lib/utils/auth'
+import { headers } from 'next/headers'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const supabase = await createClient()
-  const {
-    data: { user },
-  } = await supabase.auth.getUser()
+  const headersList = await headers()
+  const userId = headersList.get('x-user-id')
 
-  if (!user) {
+  if (!userId) {
     return redirectWithError(
       process.env.NEXT_PUBLIC_APP_URL || 'https://strank.io',
       ROUTES.PUBLIC.HOME,
@@ -23,7 +21,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <main className="min-h-[100dvh]">
       <div className="mx-auto min-h-[100dvh] w-full max-w-[450px] bg-[#FFF9FA] pt-11">
-        <UserProvider userId={user.id}>
+        <UserProvider userId={userId}>
           <PrivatePageHeader />
           {children}
           <div className="fixed bottom-0 left-1/2 flex w-full max-w-[450px] -translate-x-1/2 justify-center">
