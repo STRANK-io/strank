69580f58484810fabe84fc8629e11f71e3c8644b
sum-in-e <suminkim.me@gmail.com>
2025-01-24 21:45:02 +0900

    fix: 렌더링 개선

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -3,14 +3,16 @@ import PrivatePageNav from '@/components/common/PrivatePageNav'
 import { UserProvider } from '@/contexts/UserContext'
 import { ERROR_CODES } from '@/lib/constants/error'
 import { ROUTES } from '@/lib/constants/routes'
+import { createClient } from '@/lib/supabase/server'
 import { redirectWithError } from '@/lib/utils/auth'
-import { headers } from 'next/headers'
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
-  const headersList = await headers()
-  const userId = headersList.get('x-user-id')
+  const supabase = await createClient()
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
 
-  if (!userId) {
+  if (!user) {
     return redirectWithError(
       process.env.NEXT_PUBLIC_APP_URL || 'https://strank.io',
       ROUTES.PUBLIC.HOME,
@@ -21,7 +23,7 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <main className="min-h-[100dvh]">
       <div className="mx-auto min-h-[100dvh] w-full max-w-[450px] bg-[#FFF9FA] pt-11">
-        <UserProvider userId={userId}>
+        <UserProvider userId={user.id}>
           <PrivatePageHeader />
           {children}
           <div className="fixed bottom-0 left-1/2 flex w-full max-w-[450px] -translate-x-1/2 justify-center">
