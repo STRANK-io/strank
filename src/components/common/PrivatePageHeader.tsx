ff07d801fb8346779e0397d04125928a1e6aac20
sum-in-e <suminkim.me@gmail.com>
2025-01-30 20:17:59 +0900

    fix: 가입 이후 페이지 헤더 로고 선명하게 변경

--- src/components/common/PrivatePageHeader.tsx
+++ src/components/common/PrivatePageHeader.tsx
@@ -1,10 +1,13 @@
-import Image from 'next/image'
+import StrankDefaultLogo from '@/components/common/logos/StrankDefaultLogo'
+import StrankTitleLogo from '@/components/common/logos/StrankTitleLogo'
 
 export default function PrivatePageHeader() {
   return (
     <header className="flex items-center gap-[9.5px] px-5">
-      <Image src="/images/strank-logo.png" alt="스트랭크 로고" width={54.5} height={40} priority />
-      <span className="text-xl font-bold leading-[23.87px]">STRANK</span>
+      <div className="h-[40px] w-[54.5px]">
+        <StrankDefaultLogo className="h-full w-full" />
+      </div>
+      <StrankTitleLogo width={85} height={16} />
     </header>
   )
 }
