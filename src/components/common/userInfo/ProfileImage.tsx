f33f3319b4190a47ac03f579254c9a8e0be519a6
sum-in-e <suminkim.me@gmail.com>
2025-02-04 09:14:40 +0900

    fix: 프로필  이미지 안 보이는 이슈

--- src/components/common/userInfo/ProfileImage.tsx
+++ src/components/common/userInfo/ProfileImage.tsx
@@ -12,27 +12,25 @@ interface ProfileImageProps {
 
 export const ProfileImage = ({ imageUrl, size, className }: ProfileImageProps) => {
   return (
-    <>
-      <div
-        className={cn(
-          'relative flex items-center justify-center overflow-hidden rounded-full bg-white',
-          `h-[${size}px] w-[${size}px]`,
-          className
-        )}
-      >
-        {imageUrl ? (
-          <Image
-            src={imageUrl}
-            alt="User Profile Image"
-            fill
-            sizes={`${size}px`}
-            className="object-cover"
-            priority
-          />
-        ) : (
-          <DefaultUserIcon width={size} height={size} />
-        )}
-      </div>
-    </>
+    <div
+      className={cn(
+        'relative flex items-center justify-center overflow-hidden rounded-full bg-white',
+        className
+      )}
+      style={{ width: `${size}px`, height: `${size}px` }}
+    >
+      {imageUrl ? (
+        <Image
+          src={imageUrl}
+          alt="User Profile Image"
+          fill
+          sizes={`${size}px`}
+          className="object-cover"
+          priority
+        />
+      ) : (
+        <DefaultUserIcon width={size} height={size} />
+      )}
+    </div>
   )
 }
