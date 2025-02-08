ef6d222856b3954f17bbd51652c4f33573609f03
sum-in-e <suminkim.me@gmail.com>
2025-02-09 00:09:27 +0900

    fix: GTM -> GA로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import Providers from '@/components/providers'
 import { pretendardFont } from '@/lib/fonts'
 import { Metadata } from 'next'
 import { Toaster } from 'sonner'
-import { GoogleTagManager } from '@next/third-parties/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Viewport } from 'next'
 
 export const metadata: Metadata = {
@@ -34,7 +34,7 @@ export default function RootLayout({
     <html lang="ko" className={`${pretendardFont.variable} font-sans`} suppressHydrationWarning>
       <body suppressHydrationWarning className="bg-gray-100">
         {process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_GA_ID && (
-          <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GA_ID} />
+          <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
         )}
         <Providers>{children}</Providers>
         <Toaster toastOptions={{ unstyled: true }} duration={2000} />
